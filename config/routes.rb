eb9f5d9f6ab11b916fee3b32c0a12b79d03631e0
Toshiki Isawa <jack_the_fool@outlook.com>
2020-11-09 21:24:48 +0900

    Hello Vue

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   get 'top/top'
+  root to: 'top#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    #API用のルーティングはここに書く
+  end
+
+  get '*path', to: 'top#top', format: false   #getのリクエストをまとめる
 end
